5000d959c1bb14858a45c16869d906ca2492ae0c
zhuang23334 <137919127+zhuang23334@users.noreply.github.com>
2023-11-27 01:12:02 +0800

    oj1.cpp

--- 源2.cpp
+++ 源2.cpp
@@ -1,124 +1,41 @@
-#include<stdlib.h>
-#include<stdio.h>
-#include<queue>
-#pragma warning(disable:4996)
+#include<iostream>
+#pragma warning(disable: 4996)
 using namespace std;
-struct tnode {
-	long long int data;
-	struct tnode* parent;
-	struct tnode* Lchildren;
-	struct tnode* Rchildren;
-};
-tnode* createnode(long long int data)
-{
-	struct tnode* newNode = (struct tnode*)malloc(sizeof(struct tnode));
-	newNode->data = data;
-	newNode->Lchildren = NULL;
-	newNode->Rchildren = NULL;
-	return newNode;
-}
 int main()
 {
-	long long int order = 1;
-	long long int lost[132] = { 0 };
-	long long int target[132] = { 0 };
-	int m, n;
-	int j = 0;
-	int k = 0;
-	int ff = 0;
-	queue<tnode*>q;
-	scanf("%d", &n);
-	scanf("%d", &m);
-	if (n != 0)
-	{
-		for (int i = 0; i < n; i++)
-		{
-			scanf("%lld", &lost[i]);
-		}
-	}
-	for (int jj = 0; jj < m; jj++)
-	{
-		scanf("%lld", &target[jj]);
-	}
-	long long int M = target[m - 1];
-	struct tnode* root;
-	root = createnode(order);
-	root->parent = NULL;
-	q.push(root);
-	if (lost[0] == 1)
-	{
-		if (target[0] == 1)
-		{
-			printf("%d\n", 1);
-			for (int i = 0; i < m - 1; i++)
-			{
-				printf("%d\n", 0);
-			}
-		}
-		else
-		{
-			for (int i = 0; i < m; i++)
-			{
-				printf("%d\n", 0);
-			}
-		}
-		order = 2 * M + 1;
-	}
-
-	else
-	{
-		if (target[0] == 1)
-		{
-			printf("%d\n", 1);
-			k++;
-			ff++;
-		}
-
-		while ((!q.empty()) && (order <= 2 * M))
-		{
-			long long int currentlost = lost[j];
-			long long int currenttarget = target[k];
-			tnode* now = q.front();
-			if (now->data != currentlost)
-			{
-				order++;
-				now->Lchildren = createnode(order);
-				order++;
-				now->Rchildren = createnode(order);
-				now->Lchildren->parent = now;
-				now->Rchildren->parent = now;
-			}
-			else if (now->data == currentlost)
-			{
-				j++;
-			}
-			if (now->data == currenttarget)
-			{
-				long long int ch[200] = { 0 };
-				int rt = 0;
-				tnode* ref = now;
-				while (ref->parent != NULL)
-				{
-					ch[rt] = ref->data;
-					rt++;
-					ref = ref->parent;
-				}
-				ch[rt] = 1;
-				for (int pb = rt; pb >= 0; pb--)
-				{
-					printf("%lld ", ch[pb]);
-				}
-				printf("\n");
-				k++;
-				ff++;
-			}
-			q.pop();
-			if (now->Lchildren != NULL) q.push(now->Lchildren);
-			if (now->Rchildren != NULL) q.push(now->Rchildren);
-		}
-		for (int i = 0; i < m - ff; i++)
-		{
-			printf("%d\n", 0);
-		}
-	}
+    std::ios::sync_with_stdio(false);
+    int N;
+    cin >> N;//输入学生人数
+    int a;//定义缓存整型数
+    int b[10][10] ={0};//定义二维数组，行代表输入值的位序号（亿……十、个位），列代表该位置的数字（0、1、2……9）
+    int d[10] = { 0 };//定义最终输出学号的数字序列，每一个数组成员的值代表其对应位序数值
+    int p=0;//初始化最终输出学号
+    for (int i = 0; i < 3 * N - 1; i++)
+    {
+        cin >> a;
+        int c[10] = { 0 };
+        int M = 1000000000;
+        for (int j = 0; j < 10; j++)
+        {
+            c[j] = a/ M;
+            a = a - M * c[j];
+            M = M / 10; //利用c[]数组提取出输入学号的每一位数
+            b[j][c[j]]++;//b数组对应位置加一
+        }
+    }
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            if (b[i][j] % 3 != 0)//找出每一位序中不能被3整除的数值（说明此数值有被加过两次而不是三次，也就是说该值就是缺失学号该位置的数值）
+                d[i] = j;//（提取出缺失学号的数字序列）
+        }
+    }
+    for (int i = 0; i < 10; i++)
+    { 
+        p *= 10;
+        p += d[i];//求出该缺失学号
+    }
+    cout << p;
+    return 0;
 }
